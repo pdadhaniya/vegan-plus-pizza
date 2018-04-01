7a1e5592621ebb5fe79fff491354ea99a73d2d45
Parag Dadhaniya <paragdadhaniya@gmail.com>
2018-04-01 18:48:41 -0500

    Twitter link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import twitterLogo from './Twitter_Social_Icon_Circle_Color.svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Main-container">
+          <div className="Twitter-info">
+            <img src={twitterLogo} className="Twitter-logo" alt="twitterlogo" />
+            <a href="https://www.twitter.com/veganpluspizza">VeganPlusPizza</a>
+          </div>
+        </div>
       </div>
     );
   }
